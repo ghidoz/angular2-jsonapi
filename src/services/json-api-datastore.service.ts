92feecba2b2e3864c32c89e7c8865c2bf2242683
Clemens John <clemens.john@floh1111.de>
2016-09-19 10:22:52 +0200

    Make errorMsg string again (typing got lost on merge accidentally)
    Signed-off-by: Clemens John <clemens.john@floh1111.de>

--- src/services/json-api-datastore.service.ts
+++ src/services/json-api-datastore.service.ts
@@ -143,7 +143,7 @@ export class JsonApiDatastore {
     }
 
     protected handleError(error: any) {
-        let errMsg = (error.message) ? error.message :
+        let errMsg: string = (error.message) ? error.message :
             error.status ? `${error.status} - ${error.statusText}` : 'Server error';
         console.error(errMsg);
         return Observable.throw(errMsg);
