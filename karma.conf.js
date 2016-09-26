de5b21b365c851e2c25eee55ebfad0e9a9023249
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2016-09-26 20:05:31 +0200

    Add karma scaffolding

---
+++ karma.conf.js
@@ -0,0 +1,55 @@
+module.exports = function (config) {
+  var _config = {
+    basePath: '',
+    frameworks: ['jasmine'],
+
+    plugins: [
+      require('karma-webpack'),
+      require('karma-sourcemap-loader'),
+      require('karma-jasmine'),
+      require('karma-phantomjs-launcher'),
+      require('karma-spec-reporter')
+    ],
+
+    files: [
+      {pattern: './karma-test-shim.conf.js', watched: false}
+    ],
+
+    preprocessors: {
+      './karma-test-shim.conf.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: {
+      resolve: {
+        root: __dirname,
+        extensions: ['', '.ts', '.js', '.json'],
+      },
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          {
+            test: /\.ts$/,
+            loader: 'ts',
+            exclude: [/node_modules/]
+          }
+        ]
+      },
+      stats: { colors: true, reasons: true },
+      debug: false
+    },
+
+    webpackServer: {
+      noInfo: true
+    },
+
+    reporters: ['spec'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false
+  };
+
+  config.set(_config);
+};
