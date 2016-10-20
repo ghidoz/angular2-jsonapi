2f09e5915505ed3973efb92596da8b834f1a4526
Clemens John <clemens.john@floh1111.de>
2016-10-20 15:34:56 +0200

    Fix indentation
    Signed-off-by: Clemens John <clemens.john@floh1111.de>

--- src/models/document.model.ts
+++ src/models/document.model.ts
@@ -1,22 +1,22 @@
 import { LinksModel } from './links.model';
 
 export class DocumentModel<T> {
-    _links: LinksModel = new LinksModel;
-    _data: T;
+  _links: LinksModel = new LinksModel;
+  _data: T;
 
-    constructor(body: any) {
-        this._links.updateLinks(body.links);
-    }
+  constructor(body: any) {
+    this._links.updateLinks(body.links);
+  }
+  
+  public links(name: string = null) {
+    return this._links.links(name);
+  }
 
-    public links(name: string = null) {
-        return this._links.links(name);
-    }
+  public data(): T {
+    return this._data;
+  }
 
-    public data(): T {
-        return this._data;
-    }
-
-    public setData(data: any) {
-        this._data = data;
-    }
+  public setData(data: any) {
+    this._data = data;
+  }
 }
