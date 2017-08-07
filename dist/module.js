d07bc64d37ccf9df54ce826678c7620886434b5a
Mihael Safaric <mihael.safaric@gmail.com>
2017-08-07 02:20:34 +0200

    Fix linter errorrs

--- dist/module.js
+++ dist/module.js
@@ -1,10 +1,4 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var http_1 = require("@angular/http");
@@ -12,13 +6,15 @@ var providers_1 = require("./providers");
 var JsonApiModule = (function () {
     function JsonApiModule() {
     }
+    JsonApiModule.decorators = [
+        { type: core_1.NgModule, args: [{
+                    providers: [providers_1.PROVIDERS],
+                    exports: [http_1.HttpModule]
+                },] },
+    ];
+    /** @nocollapse */
+    JsonApiModule.ctorParameters = function () { return []; };
     return JsonApiModule;
 }());
-JsonApiModule = __decorate([
-    core_1.NgModule({
-        providers: [providers_1.PROVIDERS],
-        exports: [http_1.HttpModule]
-    })
-], JsonApiModule);
 exports.JsonApiModule = JsonApiModule;
 //# sourceMappingURL=module.js.map
---
+++ typings/index.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="globals/zone.js/index.d.ts" />
