d827ddff9135ada477871812ad6856f51e25d52f
Clemens John <clemens.john@openitb.com>
2018-02-18 11:27:00 +0100

    Allow passing a single Object to JsonApiDocument and add Method getModel() for retrieving it.
    Signed-off-by: Clemens John <clemens.john@openitb.com>

--- src/models/json-api-document.ts
+++ src/models/json-api-document.ts
@@ -1,11 +1,27 @@
 export class JsonApiDocument<T> {
-  constructor(protected jsonApiModels: Array<T>, protected metaData?: any) {}
+  constructor(protected data: Array<T> | T, protected meta?: any) {}
 
-  public getModels(): T[] {
-    return this.jsonApiModels;
+  public getModels() : T[] {
+    if (!(this.data instanceof Array)) {
+      throw new Error('Data is not an array.');
+    }
+
+    return this.data;
+  }
+
+  public getModel() : T {
+    if (this.data instanceof Array) {
+      throw new Error('Data is not an object.');
+    }
+
+    return this.data;
+  }
+
+  public getData(): T[] | T {
+    return this.data;
   }
 
   public getMeta(): any {
-    return this.metaData;
+    return this.meta;
   }
 }
