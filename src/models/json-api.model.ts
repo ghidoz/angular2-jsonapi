5c12f0fa131b9dee59ae7824823fb461a0977b74
Jacob Bullock <jacob.bullock@gmail.com>
2018-05-25 08:35:06 -0700

    removing commented, unused approach

--- src/models/json-api.model.ts
+++ src/models/json-api.model.ts
@@ -203,28 +203,4 @@ export class JsonApiModel {
 
     return newObject;
   }
-
-  // public static deserialize<T extends JsonApiModel>(modelType: ModelType<T>, data: any, datastore: JsonApiDatastore) {
-  //   console.log('JsonApiModel.deserialize');
-  //   data.attributes = JsonApiModel.transformSerializedNamesToPropertyNames(modelType, data.attributes);
-  //   return new modelType(datastore, data);
-  // }
-  //
-  // // tslint:disable-next-line:max-line-length
-  // protected static transformSerializedNamesToPropertyNames<T extends JsonApiModel>(modelType: ModelType<T>, attributes: any) {
-  //   const serializedNameToPropertyName = JsonApiModel.getModelPropertyNames(modelType.prototype);
-  //   const properties: any = {};
-  //
-  //   Object.keys(serializedNameToPropertyName).forEach((serializedName) => {
-  //     if (attributes[serializedName] !== null && attributes[serializedName] !== undefined) {
-  //       properties[serializedNameToPropertyName[serializedName]] = attributes[serializedName];
-  //     }
-  //   });
-  //
-  //   return properties;
-  // }
-  //
-  // protected static getModelPropertyNames(model: JsonApiModel) {
-  //   return Reflect.getMetadata('AttributeMapping', model);
-  // }
 }
