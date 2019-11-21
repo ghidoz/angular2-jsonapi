2bcb89f75d121e18adc9dc9dc4b1255ce9fc5665
Frans Saris <frans@beech.it>
2019-11-21 11:18:56 +0100

    [BUGFIX] Unmask null value Date as null

---
+++ projects/angular2-jsonapi/src/converters/date/date.converter.spec.ts
@@ -0,0 +1,38 @@
+import { DateConverter } from './date.converter';
+import { parseISO } from 'date-fns';
+
+describe('Date converter', () => {
+  const converter: DateConverter = new DateConverter();
+
+  describe('mask method', () => {
+
+    it('Null stays null', () => {
+        const value = converter.mask(null);
+        expect(value).toBe(null);
+    });
+
+    it ( 'string is transformed to Date object', () => {
+      const value = converter.mask('2019-11-11');
+      expect(value instanceof Date).toBeTruthy();
+    });
+
+    it ( 'empty string is transformed to Date object', () => {
+      const value = converter.mask('');
+      expect(value instanceof Date).toBeTruthy();
+    });
+  });
+
+  describe('unmask method', () => {
+
+    it('Null stays null', () => {
+        const value = converter.unmask(null);
+        expect(value).toBeNull();
+    });
+
+    it ( 'Date to be transformed to string', () => {
+      const value = converter.unmask(parseISO('2019-11-11'));
+      expect(typeof value === 'string').toBeTruthy();
+    });
+  });
+
+});
